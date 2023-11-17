55255539c59ced3f6fdbb49e7442f2ef94b58aa0
Lenny Garnier <lenny.garnier00@gmail.com>
2023-11-17 13:21:09 +0900

    syntax

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -10,8 +10,7 @@ const Navbar = ({setUserData}) => {
   const [loading, setLoading] = useState(false);
 
   return (
-    <Flex justifyContent={'space-between'} py={6} alignItems={"Center"} borderBottomWidth={2} borderColor='#2EA043'
-      backgroundColor='#000' zIndex={10} position='fixed' top={0} width='container.lg' >
+    <Flex justifyContent={'space-between'} py={6} alignItems={"Center"} borderBottomWidth={2} borderColor='#2EA043' backgroundColor='#000' zIndex={10} position='fixed' top={0} width='container.lg' >
         <box>
             <Image src={"/logo.svg"} fill alt='github-logo' sx={{filter: "invert(1)", height: "75px"}} />
         </box>
